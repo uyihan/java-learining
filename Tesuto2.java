b84fec7e872eb1eeb18aa490ccfbca0c5f5ada9a
uyihan <uyihan24@icloud.com>
2026-03-24 19:20:59 +0800

    Add files via upload

--- Tesuto2.java
+++ Tesuto2.java
@@ -1,22 +1,47 @@
 /**
- * 设计一个方法求数组的最大值，并将最大值返回
- * 2026.3.11
+ * 数字加密
+ * 2026.3.18
  * 吴宇航
  */
+
+import java.util.Scanner;
+
 public class Tesuto2 {
+    //某系统的数字加密（大于0），采用加密方式进行传输
+    //规则：先得到每位数，然后每位数都加上5，在对10求余，最后将所有数字反转，得到一串新数
     public static void main(String[] args) {
-        int[] hairetsu = {1, 3, 5, 6, 2, 9};
-        hairetsu_hyouji(hairetsu);
-        int max =hairetsu_hyouji(hairetsu);
-        System.out.println("最大值为:"+max);
-    }
-    public static int hairetsu_hyouji(int[] hairetsu){
-        int max = hairetsu[0];
-        for (int i = 1; i < hairetsu.length; i++) {
-            if (hairetsu[i] > max) {
-                max = hairetsu[i];
-            }
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int count = 0;
+        int temp = number;
+        while (number != 0) {
+            number = number / 10;
+            count++;
         }
-            return max;
+        int[] arr = new int[count];
+        int index = arr.length - 1;
+        while (temp != 0) {
+            int ge = temp % 10;
+            temp = temp / 10;
+            arr[index] = ge;
+            index--;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i] + 5;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i] % 10;
+        }
+        int number0 = 0;
+        for (int i = 0, j = arr.length - 1; i < j; i++, j--) {
+            int temp0 = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp0;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            number0 = number0 * 10 + arr[i];
+        }
+        System.out.println("加密后的数为:" + number0);
     }
 }
+
